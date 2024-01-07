77214d7bad83d2ce7aeb0f809ebe5570529e5afa
Vallejo <119472051+Vallejo8@users.noreply.github.com>
2024-01-07 16:11:06 -0600

    Operaciones bang y minus resuletas

--- src/auxiliares/ExprUnary.java
+++ src/auxiliares/ExprUnary.java
@@ -11,8 +11,26 @@ public class ExprUnary extends Expression{
         this.right = right;
     }
 
-    @Override
+      @Override
     public Object solve(){
-        return null;
+        switch (operator.tipo) {
+            case BANG:
+                return !isTruthy(right.solve());
+            case MINUS:
+                Object rightValue = right.solve();
+                if(!(rightValue instanceof Double)){
+                    throw new RuntimeException("El operando debe ser un número para el operador unario '" + operator.lexema + "'.");
+                }
+                return -((Double)rightValue);
+            default:
+                throw new RuntimeException("Operador no soportado: " + operator.lexema);
+        }
+    }
+
+    //Función auxiliar para verificar si un valor es verdadero (truthy)
+    private boolean isTruthy(Object value) {
+        if (value == null) return false;
+        if (value instanceof Boolean) return (boolean) value;
+        return true;
     }
 }
